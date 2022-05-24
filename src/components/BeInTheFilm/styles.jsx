b0be07fef6010825a78ba8b724fd8eb1d82b65bf
scarlos723 <carlossde58@gmail.com>
2022-05-24 10:43:22 -0500

    fix styles in be in the film

--- src/components/BeInTheFilm/styles.jsx
+++ src/components/BeInTheFilm/styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { ResponsiveTo } from '../../hooks/useResponsive'
 
 export const TextBox = styled.div`
   width: 90%;
@@ -6,5 +7,18 @@ export const TextBox = styled.div`
   margin: 85px auto;
   p{
     text-align: center;
+  }
+  h2{
+    margin-bottom: 11px;
+  }
+  ${ResponsiveTo('lg')}{
+    h3{
+      margin-bottom: 25px;
+    }
+  }
+  ${ResponsiveTo('xl')}{
+    h4{
+      margin-bottom: 14px;
+    }
   }
 `
--- src/components/Synopsis/styles.jsx
+++ src/components/Synopsis/styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { ResponsiveTo } from '../../hooks/useResponsive'
 
 export const TextBox = styled.div`
   width: 90%;
@@ -10,6 +11,24 @@ export const TextBox = styled.div`
       strong{
         text-shadow: 0px 0px 1px #F10505;
       }
+    }
+  }
+  h3{
+    margin-bottom: 23px;
+  }
+  ${ResponsiveTo('md')}{
+    h3{
+    margin-bottom: 31px;
+    }
+  }
+  ${ResponsiveTo('lg')}{
+    h3{
+    margin-bottom: 44px;
+    }
+  }
+  ${ResponsiveTo('xl')}{
+    h3{
+    margin-bottom: 27px;
     }
   }
 `
