e63eab33698663a761ea0e5adc877f2bb72db2aa
scarlos723 <carlossde58@gmail.com>
2022-05-24 09:59:08 -0500

    fix icons paper

--- src/components/Participate/styles.jsx
+++ src/components/Participate/styles.jsx
@@ -34,8 +34,19 @@ export const Box = styled.div`
       cursor: pointer;
       width: 87.85px;
       height: 141.05px;
+      gap:15px;
+      h3{
+        font-family: "steinbeckregular";
+        font-size: 12px;
+        transition: all 0.3s;
+      }
       img{
         width: 100%;
+      }
+      &:hover{
+        h3{
+          color: #12D466;
+        }
       }
     }
   }
@@ -47,13 +58,27 @@ export const Box = styled.div`
       article{
         width: 116.01px;
         height: 186.27px;
+        gap:20px;
+        
       }
     }
   }
   ${ResponsiveTo('lg')}{
     max-width: 684px;
+    .icons-container{
+      article{
+        gap:22px
+      }
+    }
   }
   ${ResponsiveTo('xl')}{
     max-width: 826px;
+    .icons-container{
+      article{
+        h3{
+          font-size: 14px;
+        }
+      }
+    }
   }
 `
