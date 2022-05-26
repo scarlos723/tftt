c8d0c3c3ca19742e2cc00f80aeb4dd356f356231
Yennifer Herrera <yenniferh@uninorte.edu.co>
2022-05-26 11:47:38 -0500

    ADD: Seo to the Register and Terms pages

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,11 +1,15 @@
 import Banner from '@/components/BannerRegister'
 import Form from '@/components/FormRegister'
 import Stepper from '@/components/Stepper'
+import Seo from '@/components/Seo'
 
-// TODO: Add SEO component
 function Register() {
   return (
     <main>
+      <Seo
+        title="Register"
+        description='Register to be part of the team, in TALES FROM THE TRAP and stay updated'
+      />
       <Banner>
         <Stepper />
       </Banner>
