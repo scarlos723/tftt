4741df3bcbef0eb03faa986572e2136f1b51ea37
scarlos723 <carlossde58@gmail.com>
2022-06-06 12:10:15 -0500

    add link to dashboard in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,7 @@ import { Contain, Container, ImgTFTT } from './styles'
 import logo from '@/logos/logoTFTT.svg'
 import imgTftt from '@/images/footer/TFTT.png'
 import imgFade from '@/images/footer/FADE.png'
+import { Link } from 'react-router-dom'
 export default function Footer () {
   return (
     <Container>
@@ -18,6 +19,9 @@ export default function Footer () {
         <div>
           <p> DarkHorse Film Studios All Rights Reserved.</p>
           <img src={logo} alt="" />
+
+          <Link to='/dashboard' style={{ textDecoration: 'none' }}><p style={{ fontSize: 8 }}>Go to dashboard(only admin)</p></Link>
+
         </div>
       </Contain>
     </Container>
