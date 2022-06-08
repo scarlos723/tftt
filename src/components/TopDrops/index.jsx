a7d228541b1ccea8bf0ecbb926e2d26d8ef35f50
scarlos723 <carlossde58@gmail.com>
2022-06-08 17:23:44 -0500

    fix issue7 and issue 8

--- src/components/TopDrops/index.jsx
+++ src/components/TopDrops/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-  Article,
+  // Article,
   CardsContainer,
   Container,
   // RowButton,
@@ -10,9 +10,7 @@ import {
 } from './styles'
 import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/logos/logoTFTT.svg'
-import picture1 from '@/images/marketplace/picture1.png'
-import picture2 from '@/images/marketplace/picture2.png'
-import picture3 from '@/images/marketplace/picture3.png'
+
 export default function TopDrops () {
   const navigate = useNavigate()
   return (
@@ -23,30 +21,10 @@ export default function TopDrops () {
       </Title>
       <Section>
         <CardsContainer>
-          <Article >
-            <img src={picture1} alt="picture singer man" />
-            <div className="hover">
-              <h3>purchase a <br /> roll in the film</h3>
-            </div>
-          </Article>
-          <Article >
-            <img src={picture2} alt="picture singer girls" />
-            <div className="hover">
-              <h3>
-                purchase the opportunity to walk in the red carpet
-              </h3>
-            </div>
-          </Article>
-          <Article >
-            <img src={picture3} alt="picture man in to scene" />
-            <div className="hover">
-              <h3>
-                purchase the opportunity to meet the cast members
-              </h3>
-            </div>
-          </Article>
+          <article />
+          <article />
+          <article />
         </CardsContainer>
-
       </Section>
       <TextBox>
         <h3><span>Don’t miss any detail!</span></h3>
