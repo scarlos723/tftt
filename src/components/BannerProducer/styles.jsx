018d03c9ae331f4362e2df8ef297629072ab1de7
scarlos723 <carlossde58@gmail.com>
2022-06-14 12:08:09 -0500

    fix banner producer

--- src/components/BannerProducer/styles.jsx
+++ src/components/BannerProducer/styles.jsx
@@ -3,11 +3,12 @@ import bgImg from '../../assets/images/producer/bgBanner.png'
 import { ResponsiveTo } from '../../hooks/useResponsive'
 
 export const Background = styled.div`
-  background: url(${bgImg});
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0) 4.97%, #000000 83.77%),
+          url(${bgImg});
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size:100% 40%, cover;
   background-position-x: right;
-  background-position-y: top;
+  background-position-y:bottom, top;
   display: grid;
   place-content: center;
   height: 422px;
