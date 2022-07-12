e1a2278176de3f0bf0c928707f08a9b1c3030bbe
Yennifer Herrera <yenniferh@uninorte.edu.co>
2022-07-12 18:52:06 -0500

    ADD: Countdown for Payment without styling

--- src/pages/Payment2/index.jsx
+++ src/pages/Payment2/index.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import PaymentCheckoutForm from '../../components/PaymentCheckoutForm'
 import Seo from '../../components/Seo'
-export default function Payment2 () {
+import { addMinutes, intervalToDuration } from 'date-fns'
+export default function Payment2() {
+  const counterRef = useRef(null)
+  useEffect(() => {
+    if (!counterRef.current) return
+    let futureTime = addMinutes(Date.now(), 2)
+    let interval = setInterval(() => {
+      const elapsedTime = intervalToDuration({
+        start: Date.now(),
+        end: futureTime,
+      })
+      counterRef.current.innerHTML = `${elapsedTime.minutes}:${elapsedTime.seconds}`
+      if (elapsedTime.minutes === 0 && elapsedTime.seconds === 0) {
+        futureTime = addMinutes(Date.now(), 2)
+        // Execute your code here
+      }
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
   return (
-    <div>
-      <Seo title="Checkout Payment"/>
+    <>
+      <Seo title="Checkout Payment" />
+      <h1 ref={counterRef} />
       <PaymentCheckoutForm />
-    </div>
+    </>
   )
 }
